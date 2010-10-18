8fbc5738f4e8ed6bbbd716f09cb58ea20b749e1c
Ash Skutches and John Marino <developers+ash+john@westarete.com>
2010-10-17 22:51:36 -0400

    Character crud.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Matchups::Application.routes.draw do
+  resources :characters
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20101018014105_add_name_to_characters.rb
@@ -0,0 +1,9 @@
+class AddNameToCharacters < ActiveRecord::Migration
+  def self.up
+    add_column :characters, :name, :string
+  end
+
+  def self.down
+    remove_column :characters, :name
+  end
+end
