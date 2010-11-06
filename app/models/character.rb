2cfc7377a1c9e0fe347b74ce9c9fc34709f1469c
Ash Skutches <ash@westarete.com>
2010-11-06 19:01:14 -0400

    Add the ability to generate a character list.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,18 @@
 class Character < ActiveRecord::Base
+  has_many :matchups, :foreign_key => 'player_id'
+  has_many :matchups, :foreign_key => 'opponent_id'
+  
+  def character_list
+    @list_of_characters = [
+      'Ryu','Ken','Adon','Sagat','Chun-Li','Blanka','E. Honda','Guile','Dhalsim',
+      'Zangief','Balrog','Vega','M. Bison','T. Hawk','Cammy','Fei Long','Dee Jay',
+      'Akuma','Guy','Rose','Sakura','Dan','Cody','Dudley','Ibuki','Makoto',
+      'C. Viper','Abel','El Fuerte','Rufus','Seth','Gouken','Juri','Hakan']
+      
+    @list_of_characters.each do |character|
+      list_char = Character.new(:name => character)
+      list_char.save
+    end
+    
+  end
 end
