d20517bc6426f778b9968b310c03e37af840ae83
Ash Skutches <ash@westarete.com>
2010-11-08 01:23:52 -0500

    Generating all characters wasn't working properly.

--- app/models/matchup.rb
+++ app/models/matchup.rb
@@ -5,11 +5,13 @@ class Matchup < ActiveRecord::Base
   
   def all_possible_matchups
     all_chars = Character.all
+    all_chars2 = Character.all
     all_chars.each do |player|
-      all_chars.each do |opponent|
+      all_chars2.each do |opponent|
         match = Matchup.new(:player => player, :opponent => opponent)
         match.save!
       end
     end
   end
+
 end
