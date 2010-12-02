02eaf55ae184cfca8a74994b4c367bedb22e7714
Ash Skutches and Justin Fedick <developers+ash+justin@westarete.com>
2010-12-02 00:25:46 -0500

    Articles can be posted right on the matchup.

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -1,9 +1,6 @@
 class MatchupsController < ApplicationController
   def show
     @match    = Matchup.find(params[:id])
-    # @player   = Character.find(params[:player])
-    # @opponent = Character.find(params[:opponent])
-    # @articles  = @match.articles.all
-    # @article  = @match.articles.new
+    @article  = @match.articles.new
   end
 end
