fad00d1c6f2d39079bfa17cf66f20b27faa8be1f
Ash Skutches <developers+ash@westarete.com>
2010-12-07 14:20:45 -0500

    Add a recent activity page.

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -1,4 +1,8 @@
 class MatchupsController < ApplicationController
+  def index
+    @matches = Matchup.all
+  end
+
   def show
     @match    = Matchup.find(params[:id])
     @article  = @match.articles.new
