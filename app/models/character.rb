b1aa1ac6abf99b243ff3ea1c8c3da21c65da85cd
Ash Skutches <developers+ash@westarete.com>
2010-12-08 19:47:28 -0500

    Generating characters did not have access to array.

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,13 +2,14 @@ class Character < ActiveRecord::Base
   has_many :matchups, :foreign_key => 'player_id'
   has_many :matchups, :foreign_key => 'opponent_id'
 
+
+  def character_list
+
   @list_of_characters = [
     'Ryu','Ken','Adon','Sagat','Chun-Li','Blanka','E. Honda','Guile','Dhalsim',
     'Zangief','Balrog','Vega','M. Bison','T. Hawk','Cammy','Fei Long','Dee Jay',
     'Akuma','Guy','Rose','Sakura','Dan','Cody','Dudley','Ibuki','Makoto',
     'C. Viper','Abel','El Fuerte','Rufus','Seth','Gouken','Juri','Hakan']
-
-  def character_list
 
     @list_of_characters.each do |character|
       list_char = Character.new(:name => character)
