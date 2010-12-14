17528ca9a62c89169268e47e3612796ee58a30e5
Ash Skutches <developers+ash@westarete.com>
2010-12-14 01:14:19 -0500

    Most recent match articles go on top.

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -6,5 +6,6 @@ class MatchupsController < ApplicationController
   def show
     @match    = Matchup.find(params[:id])
     @article  = @match.articles.new
+    @articles = @match.articles.find(:all, :order => "created_at DESC")
   end
 end
