1935183b6d1b5ad4c0927d03ecb03b97a76cc506
Ash Skutches <developers+ash@westarete.com>
2010-12-15 22:17:43 -0500

    WIP

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,4 +16,9 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to(match_path(params[:match_id]))
+  end
 end
--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -8,4 +8,13 @@ class MatchupsController < ApplicationController
     @article  = @match.articles.new
     @articles = @match.articles.find(:all, :order => "created_at DESC")
   end
+
+    def destroy
+      @lifespan = Lifespan.find(params[:id])
+      @lifespan.destroy
+      flash[:success] = "Lifespan deleted."
+      redirect_to @species
+    end
+
+
 end
