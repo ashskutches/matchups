74a4d680efb8c9ee7c7657062a5c0ccd8195d883
Ash Skutches <developers+ash@westarete.com>
2011-03-22 00:12:32 -0400

    More new style.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,5 @@
 class CharactersController < ApplicationController
-  expose(:characters) { Character.all } 
+  expose(:characters) { Character.order("name ASC") } 
   expose(:character)
   expose(:matchups) { Matchup.find_all_by_player(character)  }
 
