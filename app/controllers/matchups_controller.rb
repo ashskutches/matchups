572565e4e00cb469af014940da199357e5a206be
Ash Skutches and Justin Fedick <developers+ash+justin@westarete.com>
2011-03-31 17:05:14 -0400

    Remove global variables and set up forms for new tips.

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -2,4 +2,5 @@ class MatchupsController < ApplicationController
   expose(:matchups) { Matchup.all }
   expose(:matchup)
   expose(:tips) { matchup.tips }
+  expose(:tip) { matchup.tips.new }
 end
--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,3 +1,16 @@
 class TipsController < ApplicationController
   expose(:tips) { Tip.all }
+  expose(:matchup)
+  expose(:tip)
+
+  def create
+    tip.matchup_id = matchup.id
+    tip.user = current_user
+    if tip.save
+      redirect_to(matchup_path(matchup), :notice => '>>Thanks for the tip!<<')
+    else
+      render :action => 'new', :notice => tip.errors
+    end
+  end
+
 end
