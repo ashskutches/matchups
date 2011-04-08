aadc23287c399d914211579fff950c4d7215a038
Ash Skutches and Clinton Judy <developers+ash+clint@westarete.com>
2011-04-08 13:58:32 -0400

    Username is now set from facebook.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
     if user = User.find_by_email(data["email"])
       user
     else # Create an user with a stub password. 
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20]) 
+      User.create!(:email => data["email"], :name => data["name"], :password => Devise.friendly_token[0,20]) 
     end
   end 
   
