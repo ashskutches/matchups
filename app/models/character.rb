6c307b9c74898ab7e8eb9a3daa257643738fc10f
Ash Skutches <developers+ash@westarete.com>
2011-05-05 00:50:05 -0400

    Git won't let me rename file.

--- app/models/character.rb
+++ app/models/character.rb
@@ -18,7 +18,7 @@ class Character < ActiveRecord::Base
 
   @list_of_characters = [
     'Ryu','Ken','Adon','Sagat','Chun-Li','Blanka','E. Honda','Guile','Dhalsim',
-    'Zangief','Balrog','Vega','M. Bison','T. Hawk','Cammy','Fei Long','Dee Jay',
+    'Zangief','Balrog','Vega','M. Bison','T. hawk','Cammy','Fei Long','Dee Jay',
     'Akuma','Guy','Rose','Sakura','Dan','Cody','Dudley','Ibuki','Makoto',
     'C. Viper','Abel','El Fuerte','Rufus','Seth','Gouken','Juri','Hakan']
 
