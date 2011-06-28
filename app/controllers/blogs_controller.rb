82c1ece22f45a450426c7ec39073abb457f64198
Ash Skutches and Scott Woods <developers+ash+scott@westarete.com>
2011-06-28 19:37:33 -0400

    Create the blog resource from scaffold and refactor.

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,26 @@
+class BlogsController < ApplicationController
+  expose(:blog)
+  expose(:blogs) { Blog.order("created_at desc").limit(3) }
+
+  def create
+    if blog.save
+      redirect_to blog, :notice => 'Blog was successfully created.'
+    else
+      render :action => "new", :notice => blog.errors
+    end
+  end
+
+  def update
+    if blog.update_attributes(params[:blog])
+      flash[:success] = 'Blog updated'
+      redirect_to blog_path(blog)
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    blog.destroy
+    redirect_to :back
+  end
+end
