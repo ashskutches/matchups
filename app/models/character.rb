f52b50eca3c7ee8bb7051654bb6457562f30b28f
Ash Skutches <developers+ash@westarete.com>
2011-07-02 16:54:17 -0400

    No longer need _ in character names.

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,10 +5,10 @@ class Character < ActiveRecord::Base
   end
 
   def character_list
-   ['Ryu','Ken','Adon','Sagat','Chun_Li','Blanka','E_Honda','Guile','Dhalsim',
-    'Zangief','Balrog','Vega','M_Bison','T_hawk','Cammy','Fei_Long','Dee_Jay',
+   ['Ryu','Ken','Adon','Sagat','Chun Li','Blanka','E Honda','Guile','Dhalsim',
+    'Zangief','Balrog','Vega','M Bison','T hawk','Cammy','Fei Long','Dee Jay',
     'Akuma','Guy','Rose','Sakura','Dan','Cody','Dudley','Ibuki','Makoto',
-    'C_Viper','Abel','El_Fuerte','Rufus','Seth','Gouken','Juri','Hakan','Evil Ryu','Oni','Yang','Yun']
+    'C Viper','Abel','El Fuerte','Rufus','Seth','Gouken','Juri','Hakan','Evil Ryu','Oni','Yang','Yun']
   end
 
 end
