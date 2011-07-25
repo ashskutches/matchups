543bcb623f01c57ab294257690505a04f41c9a94
Ash Skutches <developers+ash@westarete.com>
2011-07-25 01:06:34 -0400

    Show opponents tips against your character.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,5 +3,6 @@ class CharactersController < ApplicationController
   expose(:character) { params[:name] }
   expose(:tips) { Tip.order("created_at desc").limit(5) }
   expose(:characterTips) { Tip.where(:player => character) }
+  expose(:opponentTips) { Tip.where(:opponent => character) }
 
 end
